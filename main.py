2fc57465b830eb7e549662527520ac10d1379af9
yunik maharjan <yunik.maharjan@icloud.com>
2018-04-21 21:49:26 +0545

    Update main.py
    clean up

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-from ulauncher.api.shared.event import PreferencesEvent
-from ulauncher.api.shared.event import PreferencesUpdateEvent
 from ulauncher.api.shared.item.ExtensionSmallResultItem import ExtensionSmallResultItem
 from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 
